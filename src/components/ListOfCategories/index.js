a550cc7df149203b6cc3f83e002413bd53c7d83d
Carlos Sanchez Sanchez <carlos.ss@globant.com>
2019-07-30 09:16:21 -0400

    hooks y fixed listOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () => (
-  <List>
-    {
-      categories.map(category => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-csanchez-8y3yujmhm.now.sh/categories')
+      .then(result => result.json())
+      .then(data => setCategories(data))
+  }, [])
+
+  useEffect(() => {
+    const onScroll = () => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
     }
-  </List>
-)
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = fixed => (
+    <List className={fixed ? 'fixed' : ''}>
+      {
+        categories.map(category => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      }
+    </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
+  )
+}
