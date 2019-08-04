1c3d4a476b1412d61738a325be85ad9c0dceef22
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-08-04 00:04:49 -0400

    fetch of categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () => (
-  <List>
-    {
-      categories.map(category => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))
-    }
-  </List>
-)
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-csanchez.sanchezcarlos1986.now.sh/categories')
+      .then(res => res.json())
+      .then(data => setCategories(data))
+  }, [])
+
+  return (
+    <List>
+      {
+        categories.map(category => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      }
+    </List>
+  )
+}
