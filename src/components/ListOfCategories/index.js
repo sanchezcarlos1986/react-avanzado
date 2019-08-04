9496c79d3fe39cd50f8310c7a649589186182172
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-08-04 00:23:10 -0400

    scroll event listener

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://petgram-server-csanchez.sanchezcarlos1986.now.sh/categories')
@@ -11,8 +12,21 @@ export const ListOfCategories = () => {
       .then(data => setCategories(data))
   }, [])
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = () => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed])
+
+  const renderList = fixed => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => (
           <Item key={category.id}>
@@ -21,5 +35,12 @@ export const ListOfCategories = () => {
         ))
       }
     </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
