75248471fa2d5d4472adeeac0bd6842609202a31
Carlos Sanchez Sanchez <carlos.ss@globant.com>
2019-08-08 08:58:50 -0400

    customhooks

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export function useLocalStorage (key, initialValue) {
+  const [storedValue, setValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      console.log('item:', item)
+      return item !== null ? JSON.parse(item) : initialValue
+    } catch (err) {
+      return initialValue
+    }
+  })
+
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value))
+      setValue(value)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return [storedValue, setLocalStorage]
+}
