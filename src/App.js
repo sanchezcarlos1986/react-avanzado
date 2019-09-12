4dacfb0995b1d5a7ec8cbfd269dc479a6e90f081
Carlos Sanchez Sanchez <carlos.ss@globant.com>
2019-09-12 12:01:58 -0300

    reach router to navigate between categories

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Fragment } from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import React from 'react'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Home } from './pages/Home'
+
+import { Router } from '@reach/router'
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
   const detailId = urlParams.get('detail')
-  console.log('detailId:', detailId)
 
   return (
-    <Fragment>
+    <div>
       <GlobalStyle />
       <Logo />
       {
         detailId
           ? <PhotoCardWithQuery id={detailId} />
-          : <Fragment>
-            <ListOfCategories />
-            <ListOfPhotoCards categoryId={1} />
-          </Fragment>
+          : <Router>
+            <Home path='/' />
+            <Home path='/pet/:id' />
+          </Router>
       }
-    </Fragment>
+    </div>
   )
 }
