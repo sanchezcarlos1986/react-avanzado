a4791a1ab9c85e53c42682529fb1a5fcc2e96fb2
Carlos Sanchez Sanchez <carlos.ss@globant.com>
2019-09-12 12:33:07 -0300

    using Link to navigate to detail route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react'
-import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyle'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Logo } from './components/Logo'
+
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 
 import { Router } from '@reach/router'
 
-export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
-  return (
-    <div>
-      <GlobalStyle />
-      <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
-    </div>
-  )
-}
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <Logo />
+    <Router>
+      <Home path='/' />
+      <Home path='/pet/:categoryId' />
+      <Detail path='/detail/:detailId' />
+    </Router>
+  </div>
+)
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -5,6 +5,8 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
+import { Link } from '@reach/router'
+
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
@@ -15,11 +17,11 @@ export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
   return (
     <Article ref={element}>
       {show && <Fragment>
-        <a href={`/?detail=${id}`}>
+        <Link to={`/detail/${id}`}>
           <ImgWrapper>
             <Img src={src} alt='ble' />
           </ImgWrapper>
-        </a>
+        </Link>
         <ToggleLikeMutation>
           {
             (toggleLike) => {
