3ffe484445ac726cc22141ba5797e20d7f47cb60
Carlos Sanchez Sanchez <carlos.ss@globant.com>
2019-09-12 14:08:33 -0300

    protected routes

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,10 +4,10 @@ import { MdHome, MdFavoriteBorder, MdPersonOutline } from 'react-icons/md'
 
 const SIZE = '32px'
 
-export const NavBar = props => (
+export const NavBar = () => (
   <Nav>
     <Link to='/'><MdHome size={SIZE} /></Link>
-    <Link to='/fav'><MdFavoriteBorder size={SIZE} /></Link>
+    <Link to='/favs'><MdFavoriteBorder size={SIZE} /></Link>
     <Link to='/user'><MdPersonOutline size={SIZE} /></Link>
   </Nav>
 )
