95633148f54294b300081dbda42d262ded62a22a
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-09-19 18:23:57 -0300

    user in sessionStoraage

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,14 @@
 import React, { useState, createContext } from 'react'
-const Context = createContext()
+export const Context = createContext()
 
 const Provider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(false)
+  const [isAuth, setIsAuth] = useState(() => window.sessionStorage.getItem('token'))
 
   const value = {
     isAuth,
-    activateAuth: () => {
+    activateAuth: token => {
       setIsAuth(true)
+      window.sessionStorage.setItem('token', token)
     }
   }
 
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,53 +1,49 @@
-import React, { Fragment } from 'react'
-import Context from '../Context'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return (
-          <Fragment>
-            <RegisterMutation>
-              {
-                (signup, { _data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    signup({ variables })
-                      .then(activateAuth)
-                      .catch(err => `Error on signup: ${err}`)
-                  }
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
 
-                  const errorMgs = error && 'Hubo un problema'
+  return (
+    <Fragment>
+      <RegisterMutation>
+        {
+          (signup, { _data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              signup({ variables })
+                .then(({ data: { signup } }) => activateAuth(signup))
+                .catch(err => `Error on signup: ${err}`)
+            }
 
-                  return (<UserForm error={errorMgs} disabled={loading} title='Sign Up' onSubmit={onSubmit} />)
-                }
-              }
-            </RegisterMutation>
-            <hr />
-            <LoginMutation>
-              {
-                (login, { _data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    login({ variables })
-                      .then(activateAuth)
-                      .catch(err => `Error on login: ${err}`)
-                  }
+            const errorMgs = error && 'Hubo un problema'
 
-                  const errorMgs = error && 'Hubo un problema'
+            return (<UserForm error={errorMgs} disabled={loading} title='Sign Up' onSubmit={onSubmit} />)
+          }
+        }
+      </RegisterMutation>
 
-                  return (<UserForm error={errorMgs} disabled={loading} title='Log In' onSubmit={onSubmit} />)
-                }
-              }
-            </LoginMutation>
-          </Fragment>
-        )
-      }
-    }
-  </Context.Consumer>
-)
+      <LoginMutation>
+        {
+          (login, { _data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              login({ variables })
+                .then(({ data: { login } }) => activateAuth(login))
+                .catch(err => `Error on login: ${err}`)
+            }
+
+            const errorMgs = error && 'Hubo un problema'
+
+            return (<UserForm error={errorMgs} disabled={loading} title='Log In' onSubmit={onSubmit} />)
+          }
+        }
+      </LoginMutation>
+    </Fragment>
+  )
+}
