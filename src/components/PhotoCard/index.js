7b102b62998d3741e3bd02e30ecf276d972a4d6e
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-09-19 18:45:28 -0300

    JWT to do likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
@@ -9,9 +8,7 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0, liked }) => {
   const [show, element] = useNearScreen()
 
   return (
@@ -25,10 +22,7 @@ export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
         <ToggleLikeMutation>
           {
             (toggleLike) => {
-              const handleFavClick = () => {
-                !liked && toggleLike({ variables: { input: { id } } })
-                setLiked(!liked)
-              }
+              const handleFavClick = () => toggleLike({ variables: { input: { id } } })
               return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
             }
           }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,23 @@ import { App } from './App'
 import Context from './Context'
 
 const client = new ApolloClient({
-  uri: 'https://petgram-server-csanchez.sanchezcarlos1986.now.sh/graphql'
+  uri: 'https://petgram-server-csanchez.sanchezcarlos1986.now.sh/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: err => {
+    const { networkError } = err
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDOM.render(
