0229443b9b3d773cf7085fa314cde2b195dbda6c
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-09-21 15:53:12 -0300

    improvement in routing

--- src/components/ListOfFavs/index.js
+++ src/components/ListOfFavs/index.js
@@ -3,6 +3,9 @@ import { Link, Grid, Image } from './styles'
 
 export const ListOfFavs = ({ favs = [] }) => (
   <Grid>
+    {
+      !favs.length && <strong>You don't have favourites yet</strong>
+    }
     {favs.map(fav => (
       <Link key={fav.id} to={`/detail/${fav.id}`}>
         <Image src={fav.src} />
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NotFound = ({ categoryId }) => {
+  return (
+    <h1>This page doesn't exist :(</h1>
+  )
+}
